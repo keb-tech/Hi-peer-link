28bcd38dfe490389f437652dc859d9df133bb65e
keb-tech <kael.gtx@gmail.com>
2020-09-04 17:11:24 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,8 +8,8 @@ const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer({
   config: {'iceServers': [
-    { url: 'stun:stun.l.google.com:19302' },
-    { url: 'turn:numb.viagenie.ca', credential: 'muazkh', username: 'webrtc@live.com' }
+    { url: 'stun:turn01.brie.fi:5349' },
+    { url: 'turn:turn01.brie.fi:5349', credential: 'fi', username: 'brie' }
   ]}
 });
 
